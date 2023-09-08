b14110c60c372304b5f57f55eabbac1100349a17
Ayush Gupta <120300586+ayushgupta1777@users.noreply.github.com>
2023-09-08 23:31:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// let namasteBtn =document.querySelector('button');
+// namasteBtn.addEventListener('click', inputMsg);
+
+// function showMsg(){
+//     let name = prompt("enter ");
+//     namasteBtn.textContent='kesa laga' + name;
+// }
+
+function sum(a,b){
+    return a+b
+}
+document.getElementById("function").innerHTML=sum(1,2)
